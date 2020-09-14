f088e5593ac100140502a22cc545c4995b81e885
ZehraRiz <zehrataqi@gmail.com>
2020-09-14 10:54:17 +0300

    menu fixed

---
+++ main.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#toggle")
+
+
+button.addEventListener("click", () => {
+    const nav = document.querySelector("nav")
+   
+    nav.classList.toggle("open")
+     console.log(nav.classList)
+    
+})
